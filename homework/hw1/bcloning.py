95cb31f475f6fbbac6708877178ab3f600cab601
Kane Tian <kxt3@cornell.edu>
2019-08-12 17:01:13 -0700

    bcloning done

--- homework/hw1/bcloning.py
+++ homework/hw1/bcloning.py
@@ -4,13 +4,11 @@ import tensorflow as tf
 import argparse
 import os
 import numpy as np
-tf.enable_eager_execution()
 
-def make_model():
-    inputs = tf.keras.Input(shape=(111,))
-    x = tf.keras.layers.Dense(64, activation=tf.nn.relu)(inputs)
-    x = tf.keras.layers.Dense(8)(x)
-    
+def make_model(i=11, l1=64, l2=2):
+    inputs = tf.keras.Input(shape=(i,))
+    x = tf.keras.layers.Dense(l1, activation=tf.nn.relu)(inputs)
+    x = tf.keras.layers.Dense(l2)(x)
     model = tf.keras.Model(inputs=inputs, outputs=x)
     return model
     
@@ -18,7 +16,7 @@ def train():
     parser = argparse.ArgumentParser()
     parser.add_argument('pkl_file', type=str)
     parser.add_argument('ckpt', type=str)
-    parser.add_argument('--epochs', type=int, default=20)
+    parser.add_argument('--epochs', type=int, default=35)
     parser.add_argument('--lr', type=float, default=1e-4)
     parser.add_argument('--decay', type=float, default=1e-6)
     parser.add_argument('--momentum', type=float, default=0.9)
@@ -27,36 +25,35 @@ def train():
     args = parser.parse_args()
     print(args)
     
-    with tf.Session():
-        model = make_model()
-        model.compile(
-            optimizer=tf.keras.optimizers.SGD(lr=args.lr, decay=args.decay, 
-                                              momentum=args.momentum, 
-                                              nesterov=not args.no_nesterov),
-            loss='mean_squared_error',
-            metrics=['mse'])
-        print(model.summary())
+    model = make_model()
+    model.compile(
+        optimizer=tf.keras.optimizers.SGD(lr=args.lr, decay=args.decay, 
+                                          momentum=args.momentum, 
+                                          nesterov=not args.no_nesterov),
+        loss='mean_squared_error',
+        metrics=['mse'])
+    print(model.summary())
 
-        data = pickle.load(open(args.pkl_file, 'rb'))
-        x_train = data['observations'][:int(0.8*len(data['observations']))]
-        y_train = np.squeeze(data['actions'][:int(0.8*len(data['actions']))], axis=1)
-        x_val = data['observations'][int(-0.2*len(data['observations'])):]
-        y_val = np.squeeze(data['actions'][int(-0.2*len(data['actions'])):], axis=1)
+    data = pickle.load(open(args.pkl_file, 'rb'))
+    x_train = data['observations'][:int(0.8*len(data['observations']))]
+    y_train = np.squeeze(data['actions'][:int(0.8*len(data['actions']))], axis=1)
+    x_val = data['observations'][int(-0.2*len(data['observations'])):]
+    y_val = np.squeeze(data['actions'][int(-0.2*len(data['actions'])):], axis=1)
 
-        os.makedirs(args.ckpt, exist_ok=True)
-        cp_callback = tf.keras.callbacks.ModelCheckpoint(
-            os.path.join(args.ckpt, 'weights.{epoch}-{val_loss:.4f}.hdf5'),
-            save_weights_only=True,
-            verbose=1)
+    os.makedirs(args.ckpt, exist_ok=True)
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(
+        os.path.join(args.ckpt, 'weights.{epoch}-{val_loss:.4f}.hdf5'),
+        save_weights_only=True,
+        verbose=1)
 
-        history = model.fit(
-            x_train, y_train,
-            batch_size=args.batch,
-            epochs=args.epochs,
-            validation_data=(x_val, y_val),
-            callbacks=[cp_callback])
+    history = model.fit(
+        x_train, y_train,
+        batch_size=args.batch,
+        epochs=args.epochs,
+        validation_data=(x_val, y_val),
+        callbacks=[cp_callback])
 
-        return history
+    return history
         
 if __name__=='__main__':
     train()
--- homework/hw1/run_clone.py
+++ homework/hw1/run_clone.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python
 
-"""
-Code to load an expert policy and generate roll-out data for behavioral cloning.
-Example usage:
-    python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --render \
-            --num_rollouts 20
-
-Author of this script and included expert policies: Jonathan Ho (hoj@openai.com)
-"""
-
 import os
 import pickle
 import tensorflow as tf
@@ -16,23 +7,23 @@ import numpy as np
 import tf_util
 import gym
 import load_policy
+import bcloning
 
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('expert_policy_file', type=str)
+    parser.add_argument('model_weights', type=str)
     parser.add_argument('envname', type=str)
     parser.add_argument('--render', action='store_true')
     parser.add_argument("--max_timesteps", type=int)
-    parser.add_argument('--num_rollouts', type=int, default=20,
-                        help='Number of expert roll outs')
     args = parser.parse_args()
 
     print('loading and building expert policy')
     policy_fn = load_policy.load_policy(args.expert_policy_file)
     print('loaded and built')
 
-    with tf.Session():
+    with tf.Session() as sess:
         tf_util.initialize()
 
         import gym
@@ -42,15 +33,19 @@ def main():
         returns = []
         observations = []
         actions = []
-        for i in range(args.num_rollouts):
+        model = bcloning.make_model(i=111, l1=64, l2=8)
+        model.load_weights(args.model_weights)
+        print(model.summary())
+        for i in range(3):
             print('iter', i)
             obs = env.reset()
             done = False
             totalr = 0.
             steps = 0
             while not done:
-                action = policy_fn(obs[None,:])
+                obs = np.expand_dims(obs, axis=0)
                 observations.append(obs)
+                action = model.predict(obs)
                 actions.append(action)
                 obs, r, done, _ = env.step(action)
                 totalr += r
@@ -65,12 +60,6 @@ def main():
         print('returns', returns)
         print('mean return', np.mean(returns))
         print('std of return', np.std(returns))
-
-        expert_data = {'observations': np.array(observations),
-                       'actions': np.array(actions)}
-
-        with open(os.path.join('expert_data', args.envname + '.pkl'), 'wb') as f:
-            pickle.dump(expert_data, f, pickle.HIGHEST_PROTOCOL)
 
 if __name__ == '__main__':
     main()
